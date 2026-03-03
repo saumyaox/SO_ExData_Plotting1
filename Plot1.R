09c3451dfbf86866787b17976f563fcf22ead802
Saumya Oli <saumyaoli@gmail.com>
2026-03-03 15:13:53 +0530

    Base plotting assignment files

---
+++ Plot1.R
@@ -0,0 +1,54 @@
+
+#Goal : examine how the household energy usage varies over a 2 day period in Feb
+
+#Reconstruct the plots with the base plotting system
+#Plot file format is a PNG file with a width of 480 pixels and a height of 480 pixels
+
+#Code file should include code for reading the data
+
+#read the data from csv file
+#mission values are coded as "?"
+
+destfile <- file.path(".", "2025","household_power_consumption.txt")
+data <- read.csv(destfile, sep=";",na.strings="?",colClasses = "character")
+
+#get the dimensions of the dataset
+#the dataset has 2,075,259 rows and 9 columns
+dim(data)
+names(data)
+
+
+#subset the data from 2007-02-01 and 2007-02-02
+#subset the rows with date values 2007-02-01 and 2007-02-02
+data_subset <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+
+#convert the Date and Time variables to Date/Time classes
+#strptime() as.Date()
+#Combine data and time into a single character string
+data_subset$DateTime <- strptime(paste(data_subset$Date, data_subset$Time),
+                                 format="%d/%m/%Y %H:%M:%S")
+
+names(data_subset)
+
+# Columns 3 through 9 contain the power, voltage, and metering data
+data_subset[, 3:9] <- lapply(data_subset[, 3:9], as.numeric)
+
+#Global Active Power (kilowatts) vs frequency with title "Global Active Power"
+#Sampling data (Thu and Fri) vs Global Active Power (kilowatts)
+#Energy sub metering (Thu and Fri), legends, Sub_metering_1, Sub_metering_2,Sub_metering_3
+#Plot 1, Plot 2, datetime vs voltage, datetime vs Global_reactive_power
+
+#open png device
+png("plot1.png",width = 480, height=480)
+
+hist(data_subset$Global_active_power,
+     col="red", 
+     main="Global Active Power",
+     xlab = "Global Active Power (kilowatts)",
+     ylab = "Frequency")
+
+# Close the device
+dev.off()
+
+
+
